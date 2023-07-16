c9c78bcb5e28f55d06f7e3310219134b9ed33158
Walter Rodrigues <metal.walter@gmail.com>
2023-07-15 23:57:03 -0300

    Add project files.

---
+++ CRUDCadastroProdutos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUDCadastroProdutos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
