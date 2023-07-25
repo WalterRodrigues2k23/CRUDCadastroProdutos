2b1025dcdf54df1e962d437490a1b596107e8f6e
Walter Rodrigues <metal.walter@gmail.com>
2023-07-25 12:14:34 -0300

    criação do banco de dados

--- CRUDCadastroProdutos/Data/MVCDbContext.cs
+++ CRUDCadastroProdutos/Data/MVCDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace CRUDCadastroProdutos.Data
+﻿using CRUDCadastroProdutos.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDCadastroProdutos.Data
 {
-    public class MVCDbContext
+    public class MVCDbContext : DbContext
     {
+        public MVCDbContext(DbContextOptions options) : base(options)
+        {            
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
     }
 }
--- CRUDCadastroProdutos/Models/Domain/Products.cs
+++ CRUDCadastroProdutos/Models/Domain/Products.cs
@@ -3,7 +3,7 @@
     public class Products
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public double Price { get; set; }
         public DateTime ValidDate { get; set; }
     }
