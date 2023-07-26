86c77ddcf9ad84cf9a14ad46e1066b24f2dd8f02
Walter Rodrigues <metal.walter@gmail.com>
2023-07-26 17:53:16 -0300

    Chamando o banco de dados pt1

--- CRUDCadastroProdutos/Program.cs
+++ CRUDCadastroProdutos/Program.cs
@@ -1,7 +1,13 @@
+using CRUDCadastroProdutos.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<MVCDbContext>(options =>
+{
+
+});
 
 var app = builder.Build();
 
