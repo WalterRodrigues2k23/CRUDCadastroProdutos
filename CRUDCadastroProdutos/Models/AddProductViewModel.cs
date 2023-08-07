cdc0a7965a837cf56ee7af7ee49cd5865dfb24ec
Walter Rodrigues <metal.walter@gmail.com>
2023-08-07 09:47:26 -0300

    Adicionando as Views e adicionando as rotas para a execução

---
+++ CRUDCadastroProdutos/Models/AddProductViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CRUDCadastroProdutos.Models
+{
+    public class AddProductViewModel
+    {
+        public string? Name { get; set; }
+        public double Price { get; set; }
+        public DateTime ValidDate { get; set; }
+    }
+}
---
+++ CRUDCadastroProdutos/Models/UpdateProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUDCadastroProdutos.Models
+{
+    public class UpdateProductViewModel
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public double Price { get; set; }
+        public DateTime ValidDate { get; set; }
+    }
+}
